10deaa9c571d4996d799d112347938d11babf197
Firede <firede@firede.us>
2014-11-03 14:25:23 +0800

    fixed codestyle.

--- src/lite.js
+++ src/lite.js
@@ -8,36 +8,36 @@
 
 /**
  * matchMedia() polyfill - Test a CSS media type/query in JS.
- * 
+ *
  * Authors & copyright (c) 2012:
  * Scott Jehl, Paul Irish, Nicholas Zakas, David Knight. Dual MIT/BSD license
  */
 
-define(function() {
+define(function () {
 
     // native matchMedia is buggy in MIUI (Android 4.1.2 version) browsers,
     // except Androd Chrome.
-    var isAndroid = navigator.userAgent.indexOf( 'Android' ) > 0;
-    var isChrome = /Chrome\/[0-9]+/.test( navigator.userAgent );
+    var isAndroid = navigator.userAgent.indexOf('Android') > 0;
+    var isChrome = /Chrome\/[0-9]+/.test(navigator.userAgent);
 
-    if ( window.matchMedia && !( isAndroid && !isChrome ) ) {
+    if (window.matchMedia && !(isAndroid && !isChrome)) {
         return window.matchMedia;
     }
 
     /**
      * MatchMedia
-     * 
+     *
      * @type {Function}
      */
-    var matchMedia = (function() {
-        
+    var matchMedia = (function () {
+
         // For browsers that support matchMedium api
         // Android 2.3 use `window.media`
         var styleMedia = window.styleMedia || window.media;
 
-        return function( media ) {
+        return function (media) {
             return {
-                matches: styleMedia.matchMedium( media || 'all' ),
+                matches: styleMedia.matchMedium(media || 'all'),
                 media: media || 'all'
             };
         };
